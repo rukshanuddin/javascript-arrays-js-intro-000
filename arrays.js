93f15fd0247050c086a67c5c0d7a7b017ca373f6
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-09-18 00:10:56 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -8,5 +8,5 @@ function addElementToBeginningOfArray(array, element){
   return [element, ...array]
 }
 function destructivelyAddElementToBeginningOfArray(array, element){
-  array.unshift(element)
+  return {array.unshift(element)}
 }
