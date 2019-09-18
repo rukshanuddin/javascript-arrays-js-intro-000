001d2fb9699be7510bd6f89cf7638aa8f9c6b54b
Rukshan Uddin <rukshan.uddin@gmail.com>
2019-09-18 00:46:47 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -22,7 +22,6 @@ function accessElementInArray(array, index){
   return array[index]
 }
 function destructivelyRemoveElementFromBeginningOfArray(array){
-  const array = []
   array.shift()
   return array
 }
